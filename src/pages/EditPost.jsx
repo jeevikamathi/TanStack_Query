d0f1c7c186d69f50f3d04ca2cb40c4431b3d0f10
jeevikamathi <jeevimuthusamy3@gmail.com>
2025-04-18 15:09:12 +0530

    Initial setup for TanStack query was Created

---
+++ src/pages/EditPost.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PostForm from "../components/PostForm";
+const EditPost = () => {
+    return(
+        <div>
+          <PostForm />
+        </div>
+    )
+}
+export default EditPost;
